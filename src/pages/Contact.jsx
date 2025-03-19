72d9e3377046dd56a1698e637db0112a662798d8
jtb00 <jack.bedinger@msbci.com>
2025-03-19 17:01:33 -0400

    Added email functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,32 @@ import './Contact.css'
 import Footer from '../components/Footer/Footer'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "a67fded9-8b00-4e97-95cc-8ca0ee74a46d");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className='contact'>
       <div className='section-header'>
@@ -18,7 +44,7 @@ const Contact = () => {
         </div>
         <div className='col'>
           <div className='form'>
-            <form action="">
+            <form onSubmit={onSubmit}>
               <label htmlFor="name">Name</label>
               <input type="text" name='name' placeholder='Name' required/>
               <label htmlFor="email">Email</label>
@@ -27,6 +53,7 @@ const Contact = () => {
               <input type="tel" name="phone" placeholder='Phone Number' required/>
               <label htmlFor="message">Message</label>
               <textarea name="message" rows='6' placeholder='Please enter your message'></textarea>
+              <button type='submit'>Submit</button>
             </form>
           </div>
         </div>
